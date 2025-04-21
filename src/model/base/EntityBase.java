4a09bdbecb056d75a45e309bbe2ef67eb197867c
Manhddm <manhducdongyb@gmail.com>
2025-04-21 21:38:22 +0700

    Base

--- src/model/base/EntityBase.java
+++ src/model/base/EntityBase.java
@@ -7,5 +7,48 @@ import java.awt.image.BufferedImage;
 
 
 public abstract class EntityBase implements IEntity {
+    protected float x;
+    protected float y;
+    protected BufferedImage image;
+    protected Rectangle bounds;
+    public EntityBase(float x, float y, BufferedImage image) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        initHitBox();
+    }
+    protected abstract void initHitBox() ;
+    @Override
+    public float getX() {
+        return this.x;
+    }
 
+    @Override
+    public float getY() {
+        return this.y;
+    }
+
+    @Override
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    @Override
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    @Override
+    public Rectangle getHitBox() {
+        return null;
+    }
+
+    @Override
+    public void draw(Graphics g) {
+
+    }
+
+    @Override
+    public void update() {
+    }
 }
